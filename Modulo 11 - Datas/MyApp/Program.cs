8d7c4299b32f2e13fa24e8918204c4b54ac8a8f7
Pablo Alan <pabloxt14@gmail.com>
2022-05-25 19:00:00 -0300

    docs: formatting dates

--- Modulo 11 - Datas/MyApp/Program.cs
+++ Modulo 11 - Datas/MyApp/Program.cs
@@ -6,6 +6,11 @@
         {
             Console.Clear();
 
+            FormattingDates();
+        }
+
+        public static void StartingWithDate()
+        {
             // var data = new DateTime(); // data padrão = 01/01/0001 00:00:00
 
             // var data = DateTime.Now; // data atual
@@ -14,6 +19,11 @@
             var data = new DateTime(2020, 10, 12, 8, 23, 14);
 
             Console.WriteLine(data);
+        }
+
+        public static void GettingDateValues()
+        {
+            var data = DateTime.Now;
 
             // Obtendo partes da data
             Console.WriteLine(data.Year);
@@ -26,6 +36,34 @@
             Console.WriteLine((int)data.DayOfWeek);
             Console.WriteLine(data.DayOfYear);
             Console.WriteLine(data.Date);
+        }
+
+        public static void FormattingDates()
+        {
+            var date = DateTime.Now;
+
+            Console.WriteLine("# Formatando Datas");
+
+            Console.WriteLine(new string('-', Console.WindowWidth));
+
+            /*
+                - yyyy = ano com 4 digitos
+                - yy = ano com 2 digitos
+                - y = ano com o mês 
+                - M = mês por extenso
+                - MM = mês com 2 digitos
+                - dd = dia com 2 digitos
+                - d = dia/mes/ano
+                - hh = hora com 2 digitos
+                - mm = minuto com 2 digitos
+                - ss = segundo com 2 digitos
+                - ff - fracão de segundo
+                - z = timezone ou fuso horário
+
+            */
+            var dateFormatted = String.Format("{0:yyyy/MM/dd hh:mm:ss ff z}", date);
+
+            Console.WriteLine(dateFormatted);
         }
     }
 }
