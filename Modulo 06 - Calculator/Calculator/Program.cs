7e1008d6f767e1ffc65e0c834de66f9117427e9d
Pablo Alan <pabloxt14@gmail.com>
2024-04-05 16:35:12 -0300

    docs: start calculator project

---
+++ Modulo 06 - Calculator/Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine(new string('-', 70));
+
+            Console.WriteLine("Calculator");
+
+            Console.WriteLine(new string('-', 70));
+
+            Console.Write("Digite o primeiro valor: ");
+
+            double value1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(new string('-', 70));
+
+            Console.WriteLine($"Valor digitado: {value1}");
+        }
+    }
+}
