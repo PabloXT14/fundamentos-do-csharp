103d5a17a2524bd2d0bb60e36b5d70867b99c556
Pablo Alan <pabloxt14@gmail.com>
2024-04-13 17:12:32 -0300

    docs: start TextEditor project

---
+++ Modulo 08 - TextEditor/TextEditor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Open(); break;
+                case 2: New(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Open()
+        {
+
+        }
+
+        static void New()
+        {
+
+        }
+    }
+}
