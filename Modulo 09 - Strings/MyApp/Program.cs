9cc73353b930449732f36f651daa5d84c14b1022
Pablo Alan <pabloxt14@gmail.com>
2024-04-17 19:48:38 -0300

    docs: guids

---
+++ Modulo 09 - Strings/MyApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Guid (Global Unique Identifier - Identificador Unico Global) já vem por padrão no System
+
+            var id = Guid.NewGuid();
+
+            // Transformando o Guid em uma string
+            // id.ToString();
+
+            // Gerando um Guid com uma string
+            // id = new Guid("8f2f5f8f-2f5f-5f8f-8f2f-5f8f8f8f8f8f"); // Ok
+            // id = new Guid(""); // Erro
+
+            // Guid todo zerado
+            // id = new Guid();
+
+            Console.WriteLine($"id: {id}");
+            Console.WriteLine($"id cortado: {id.ToString().Substring(0, 8)}");
+        }
+    }
+}
