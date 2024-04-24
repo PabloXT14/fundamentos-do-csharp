a384788b3db28e8e118186419866cd0785cff839
Pablo Alan <pabloxt14@gmail.com>
2024-04-24 20:03:04 -0300

    docs: create draw screen method

--- Modulo 10 - Editor HTML/HtmlEditor/Menu.cs
+++ Modulo 10 - Editor HTML/HtmlEditor/Menu.cs
@@ -8,10 +8,49 @@ namespace HtmlEditor
         {
             Console.Clear();
 
-            Console.BackgroundColor = ConsoleColor.Blue; // cor de fundo
-            Console.ForegroundColor = ConsoleColor.Black; // cor do texto
+            Console.BackgroundColor = ConsoleColor.Black; // cor de fundo
+            Console.ForegroundColor = ConsoleColor.Green; // cor do texto
 
-            Console.WriteLine("EDITOR HTML");
+            DrawScreen();
+
+            // Reset color
+            Console.ResetColor();
+
+            // Keeping the console window open
+            Console.ReadLine();
+        }
+
+        public static void DrawScreen()
+        {
+            var width = 70;
+            var lineAmount = 10;
+
+            // Draw top
+            DrawBorderTop(width);
+
+            // Draw body
+            DrawScreenLine(width, lineAmount);
+
+            // Draw bottom
+            DrawBorderBottom(width);
+        }
+
+        public static void DrawBorderTop(int width)
+        {
+            Console.WriteLine("╔" + new string('═', width - 2) + "╗");
+        }
+
+        public static void DrawBorderBottom(int width)
+        {
+            Console.WriteLine("╚" + new string('═', width - 2) + "╝");
+        }
+
+        public static void DrawScreenLine(int lineWidth, int lineAmount)
+        {
+            for (int line = 0; line < lineAmount; line++)
+            {
+                Console.WriteLine("║" + new string(' ', lineWidth - 2) + "║");
+            }
         }
     }
 }
