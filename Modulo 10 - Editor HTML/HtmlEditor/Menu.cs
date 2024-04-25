c335bce844107b5f8a4d28f45a5f57b19fa3ad8b
Pablo Alan <pabloxt14@gmail.com>
2024-04-25 19:28:31 -0300

    docs: displaying the menu

--- Modulo 10 - Editor HTML/HtmlEditor/Menu.cs
+++ Modulo 10 - Editor HTML/HtmlEditor/Menu.cs
@@ -4,6 +4,8 @@ namespace HtmlEditor
 {
     public static class Menu
     {
+        static int screenWidth = 70;
+
         public static void Show()
         {
             Console.Clear();
@@ -12,6 +14,10 @@ namespace HtmlEditor
             Console.ForegroundColor = ConsoleColor.Green; // cor do texto
 
             DrawScreen();
+
+            var option = WriteOptions();
+
+            Console.WriteLine($"Opção escolhida: {option}");
 
             // Reset color
             Console.ResetColor();
@@ -22,7 +28,7 @@ namespace HtmlEditor
 
         public static void DrawScreen()
         {
-            var width = 70;
+            var width = screenWidth;
             var lineAmount = 10;
 
             // Draw top
@@ -51,6 +57,36 @@ namespace HtmlEditor
             {
                 Console.WriteLine("║" + new string(' ', lineWidth - 2) + "║");
             }
+        }
+
+        public static short WriteOptions()
+        {
+            Console.SetCursorPosition(2, 1);
+            Console.WriteLine("# Editor HTML");
+
+            Console.SetCursorPosition(2, 2);
+            Console.WriteLine(new string('-', screenWidth - 4));
+
+            Console.SetCursorPosition(2, 3);
+            Console.WriteLine("Selecione uma opção abaixo:");
+
+            Console.SetCursorPosition(2, 4);
+            Console.WriteLine("1 - Novo arquivo");
+
+            Console.SetCursorPosition(2, 5);
+            Console.WriteLine("2 - Abrir arquivo");
+
+            Console.SetCursorPosition(2, 6);
+            Console.WriteLine("0 - Sair");
+
+            Console.SetCursorPosition(2, 8);
+            Console.Write("Opção: ");
+
+            var option = short.Parse(Console.ReadLine());
+
+            Console.SetCursorPosition(2, 10);
+
+            return option;
         }
     }
 }
