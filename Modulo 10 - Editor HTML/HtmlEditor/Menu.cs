2e08892dc7baeb16719698ca99ee4fe3384d72cc
Pablo Alan <pabloxt14@gmail.com>
2024-04-26 19:19:59 -0300

    docs: application menu

--- Modulo 10 - Editor HTML/HtmlEditor/Menu.cs
+++ Modulo 10 - Editor HTML/HtmlEditor/Menu.cs
@@ -17,7 +17,7 @@ namespace HtmlEditor
 
             var option = WriteOptions();
 
-            Console.WriteLine($"Opção escolhida: {option}");
+            HandleMenuOption(option);
 
             // Reset color
             Console.ResetColor();
@@ -87,6 +87,22 @@ namespace HtmlEditor
             Console.SetCursorPosition(2, 10);
 
             return option;
+        }
+
+        public static void HandleMenuOption(short option)
+        {
+            switch (option)
+            {
+                case 1: Console.WriteLine("Editor"); break;
+                case 2: Console.WriteLine("View"); break;
+                case 0:
+                    {
+                        Console.Clear();
+                        Environment.Exit(0);
+                        break;
+                    }
+                default: Show(); break;
+            }
         }
     }
 }
