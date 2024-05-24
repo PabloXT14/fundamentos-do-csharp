6f9c2c29511a5b871a0cb450443ba2904d7656ae
Pablo Alan <pabloxt14@gmail.com>
2024-05-24 20:00:00 -0300

    docs: starting with date

---
+++ Modulo 11 - Datas/MyApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var data = new DateTime(); // data padrão = 01/01/0001 00:00:00
+
+            var data = DateTime.Now; // data atual
+
+            Console.WriteLine(data);
+        }
+    }
+}
